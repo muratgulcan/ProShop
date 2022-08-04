4a5c016194d7817bd0e39e323105c7b57ac73c48
muratgulcan <76414291+muratgulcan@users.noreply.github.com>
2022-08-04 16:12:01 +0300

    first commit

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'
-import { productListReducer } from './reducers/productReducers'
+import { productListReducer,productDetailsReducer } from './reducers/productReducers'
  
 const reducer = {
     productList: productListReducer,
+    productDetails: productDetailsReducer,
 }
 
 const store = configureStore({
