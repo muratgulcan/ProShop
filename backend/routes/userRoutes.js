1deda18ed9e2aff7580af5e24ea0fc27416b40fc
muratgulcan <76414291+muratgulcan@users.noreply.github.com>
2022-08-06 16:26:53 +0300

    first commit

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose');
 const router = express.Router()
-const {authUser,getUserProfile,registerUser} = require('../controllers/userController')
+const {authUser,getUserProfile,registerUser,updateUserProfile} = require('../controllers/userController')
 const {protect} = require('../middleware/authMiddleware')
 
 router.route('/').post(registerUser)
 router.post('/login',authUser)
-router.route('/profile').get(protect,getUserProfile)
+router.route('/profile').get(protect,getUserProfile).put(protect,updateUserProfile)
 
 
 
