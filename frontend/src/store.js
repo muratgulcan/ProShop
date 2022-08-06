87395d0c3b9404ea7708df0b27dc3fa409e813b3
muratgulcan <76414291+muratgulcan@users.noreply.github.com>
2022-08-06 15:14:04 +0300

    first commit

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,17 +2,21 @@ import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'
 import { productListReducer,productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer } from './reducers/userReducer'
  
 const reducer = {
     productList: productListReducer,
     productDetails: productDetailsReducer,
-    cart:cartReducer
+    cart:cartReducer,
+    userLogin:userLoginReducer
 }
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
 
 const initialState = {
-    cart:{cartItems:cartItemsFromStorage}
+    cart:{cartItems:cartItemsFromStorage},
+    userLogin:{userInfo:userInfoFromStorage},
 }
 
 const store = configureStore({
