f93476518cff9f58ad240a49b52764d937720f2f
muratgulcan <76414291+muratgulcan@users.noreply.github.com>
2022-08-17 16:21:09 +0300

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -19,16 +19,29 @@ if(process.env.NODE_ENV === 'development'){
 
 app.use(express.json())
 
-app.get('/', (req,res) => {
-    res.send('API is running')
-})
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
-app.use('/api/products',productRoutes)
-app.use('/api/users',userRoutes)
-app.use('/api/orders',orderRoutes)
-app.use('/api/upload',uploadRoutes)
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
 
-app.use('/uploads',express.static(path.join(path.resolve(),'/uploads')))
+app.use('/uploads', express.static(path.join(path.resolve(), '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(path.resolve(), '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(path.resolve(), 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
 app.use(notFound)
 app.use(errorHandler)
 
