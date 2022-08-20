f8a32605341fcebd868443e16189514950222c18
muratgulcan <wmuratgulcan@gmail>
2022-08-21 01:10:35 +0300

    first commit

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -9,5 +9,4 @@ router.post('/login',authUser)
 router.route('/profile').get(protect,getUserProfile).put(protect,updateUserProfile)
 router.route('/:id').delete(protect,admin,deleteUser).get(protect,admin,getUserById).put(protect,admin,updateUser)
 
-
 module.exports = router
